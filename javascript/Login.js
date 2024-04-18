ad9b97050ed95ec8201cde9abd86fbde8bb7659b
Bob005f <112066092+Bob005f@users.noreply.github.com>
2024-04-18 16:56:43 +0200

    Update Login.js

---
+++ javascript/Login.js
@@ -0,0 +1,55 @@
+function on() {
+    document.getElementById("overlay").style.display = "block";
+    document.getElementById("overlay2").style.display = "";
+  }
+  
+  function on2() {
+    document.getElementById("overlay2").style.display = "block";
+    document.getElementById("overlay").style.display = "";
+  }
+
+  function off() {
+    document.getElementById("overlay2").style.display = "";
+    document.getElementById("overlay").style.display = "";
+  }
+
+  function login() {
+
+  }
+
+
+  
+  function signUp(e) {
+    e.preventDefault();
+   
+   var signupform =    document.getElementById("signupform");
+    
+    var formData = new FormData(signupform);
+  
+    fetch("https://backend.fashion-share-bmsd21a.bbzwinf.ch/api/users", {
+      method: "POST",
+
+      body: formData,
+    })
+      .then(response => {
+      if (!response.ok) {
+        throw new Error('network returns error');
+      }
+      return response.json();
+    })
+      .then((resp) => {
+        let respdiv = document.createElement("pre");
+        respdiv.innerHTML = JSON.stringify(resp, null, 2);
+        signupform.replaceWith(respdiv);
+        console.log("resp from server ", resp);
+      })
+      .catch((error) => {
+        // Handle error
+        console.log("error ", error);
+      });
+  }
+  
+  var signupform = document.getElementById("signupform");
+  
+  signupform.addEventListener("submit", signUp);
+  
