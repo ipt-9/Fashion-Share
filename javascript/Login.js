5284d63cfda3097a8e0a77bc39639467308bdf4e
Bob005f <112066092+Bob005f@users.noreply.github.com>
2024-04-18 16:42:52 +0200

    Update Login.js

--- javascript/Login.js
+++ javascript/Login.js
@@ -1,27 +1,55 @@
 function on() {
     document.getElementById("overlay").style.display = "block";
+    document.getElementById("overlay2").style.display = "";
   }
   
   function on2() {
     document.getElementById("overlay2").style.display = "block";
+    document.getElementById("overlay").style.display = "";
   }
 
-const loginPopup = document.getElementById('loginPopup');
-const loginButton = document.getElementById('loginButton');
-const closeButton = document.getElementById('closeButton');
+  function off() {
+    document.getElementById("overlay2").style.display = "";
+    document.getElementById("overlay").style.display = "";
+  }
 
-const signupPopup = document.getElementById('signupPopup');
-const signupButton = document.getElementById('signupButton');
+  function login() {
+
+  }
 
 
-loginButton.addEventListener('click', function() {
-  loginPopup.style.display = 'block';
-});
+  
+  function signUp(e) {
+    e.preventDefault();
+   
+   var signupform =    document.getElementById("signupform");
+    
+    var formData = new FormData(signupform);
+  
+    fetch("https://backend.fashion-share-bmsd21a.bbzwinf.ch/api/users", {
+      method: "POST",
 
-signupButton.addEventListener('click', function() {
-    signupPopup.style.display = 'block';
-  });
-
-closeButton.addEventListener('click', function() {
-  loginPopup.style.display = 'none';
-});
+      body: formData,
+    })
+      .then(response => {
+      if (!response.ok) {
+        throw new Error('network returns error');
+      }
+      return response.json();
+    })
+      .then((resp) => {
+        let respdiv = document.createElement("pre");
+        respdiv.innerHTML = JSON.stringify(resp, null, 2);
+        signupform.replaceWith(respdiv);
+        console.log("resp from server ", resp);
+      })
+      .catch((error) => {
+        // Handle error
+        console.log("error ", error);
+      });
+  }
+  
+  var signupform = document.getElementById("signupform");
+  
+  signupform.addEventListener("submit", signUp);
+  
