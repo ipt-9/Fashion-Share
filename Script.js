b3f9f6357ac6996225b983b32aebca3fa24dcd48
Ian-LiuIdk <ian_liu@sluz.ch>
2024-05-22 22:08:15 +0200

    corrected merging issues

--- Script.js
+++ Script.js
@@ -1,5 +1,3 @@
-
-//Post Anzeigen glaube aber unnötige function
 function getrequest2() {
   fetch("https://backend.fashion-share-bmsd21a.bbzwinf.ch/api/posts")
     .then((response) => response.json())
@@ -9,7 +7,6 @@ function getrequest2() {
     .catch((error) => console.error('Error fetching data:', error));
 }
 
-//Post Anzeigen
 function getrequest() {
   fetch("https://backend.fashion-share-bmsd21a.bbzwinf.ch/api/posts")
     .then(response => response.json())
@@ -23,7 +20,7 @@ function getrequest() {
 }
 
 
-//Post Anzeigen
+
 function displayData(data) {
   const apiDataContainer = document.getElementById('apiData');
   const innerData = data; // Daten aus dem inneren Objekt extrahieren
@@ -35,11 +32,16 @@ function displayData(data) {
       const html = `
       <div id="Post">
       <div id="PostHeader">
-      <img id="ProfilePicture" src="./Media/postPictures/images/${profilePicture}" alt="Bild">
+  
+  <img id="ProfilePicture" src="./Media/profilePictures/${profilePicture}" alt="Bild">
+
+
       <p id="UserName">${userName}</p>
       </div>
       <div id="divpicture">
-      <img id="Picture" src="./Media/profilePictures/${innerData.image}" alt="Bild">
+  
+  <img id="Picture" src="Media/postPictures/images/${innerData.image}" alt="Bild">
+
       </div>
       
 
@@ -56,7 +58,7 @@ function displayData(data) {
       apiDataContainer.innerHTML += html;
   })
 }
-// für Post Anzeigen function
+
 function getuser(id) {
   return fetch(`https://backend.fashion-share-bmsd21a.bbzwinf.ch/api/users/${id}`)
     .then(response => response.json())
